5e4d98fe1940546c701bbb693d58b5bb83430eca
VictoriaGomez7 <49539981+VictoriaGomez7@users.noreply.github.com>
2019-06-08 20:30:20 -0500

    Update InterfazPrincipal2.blade.php

--- resources/views/DocenteInterfazPrincipal/InterfazPrincipal2.blade.php
+++ resources/views/DocenteInterfazPrincipal/InterfazPrincipal2.blade.php
@@ -12,6 +12,13 @@
 </head>
 <body>
 	@include('interfazprincipal.image')
+    
+    @if (session()->has('MsjERR'))
+        <div class="alert alert-danger" role="alert" style="width: 90%; position:  absolute;top: 43%; left: 5%;z-index: 1;">
+            <button class="close" data-dismiss="alert"><span>&times;</span></button>
+            <strong>¡ERROR! </strong>{{ session('MsjERR') }}
+        </div>
+    @endif
 
 	<header>
 		<nav class="navegacion" style="width: 90%">
