220cc914861827fcc5d9476b8e0fcef315112642
DanielSotelo <49506874+DanielSotelo@users.noreply.github.com>
2019-06-14 09:05:11 -0500

    Acomodo de FPT yBachillerato

--- resources/views/Reinscripciones/form.blade.php
+++ resources/views/Reinscripciones/form.blade.php
@@ -37,7 +37,7 @@
             </div>
 
             <div style="position: absolute;top: 62%; left: 24.9%; width: 25%;height:53%;background-color:#aaa">
-                <p><input type="number" required readonly min="1" max="9999999999" id="id" name="id" value="{{$alumno->id}}" style="font-size:105%; width: 95%"/></p>
+                <p><input type="number" required readonly pattern="[1-9]{2}[0]{2}[1]{2}[0-9]{4}" id="id" name="id" value="{{$alumno->id}}" style="font-size:105%; width: 95%"/></p>
 
                 <p><input type="text" required readonly id="nombr" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,48}" name="nombre" value="{{$alumno->Nombre_A}}"style="font-size:105%; width: 95%;" /></h1>
 
@@ -111,7 +111,7 @@
             </div>
 
             @foreach($FtOBache as $FTBH)
-            <div style="position: absolute;top: 127%; left: 5%; width: 90%;height:15%;background-color:#aaa">
+            <div style="position: absolute;top: 126%; left: 5%; width: 90%;height:15%;background-color:#aaa">
                 <p style="font-size:138%">{{('Formación para el Trabajo:')}}</p>
                 <p style="font-size:138%">{{('Bachillerato:')}}</p>
                 @if($bandera==1)
