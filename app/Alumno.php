1dcc64c0fe1db4e869f5fdce6906f778e3bb2754
roquetorres <50347044+roquetorres@users.noreply.github.com>
2019-06-17 13:54:11 -0500

    Cambios

--- app/Alumno.php
+++ app/Alumno.php
@@ -10,6 +10,6 @@ class Alumno extends Model
     protected $fillable = [
         'Numero','id', 'Nombre_A', 'Nombre_P','Nombre_M','Domicilio','Domicilio',
       'Telefono_T','Telefono_A','Poblacion','Municipio','Fecha_Nac',
-      'Edad','Email','Curp','NSS','Sexo','Semestre','Grado','Requisito_A','Requisito_B','Requisito_C','Requisito_D','Requisito_E','Requisito_F','Requisito_G','Requisito_H'
+      'Edad','Email','Curp','NSS','Sexo','Semestre','Grado','Estado','Requisito_A','Requisito_B','Requisito_C','Requisito_D','Requisito_E','Requisito_F','Requisito_G','Requisito_H'
     ];
 }
